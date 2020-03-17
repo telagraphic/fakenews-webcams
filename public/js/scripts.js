099da83f00e15ffe2416506fc91ea84d3d8f29de
telagraphic <telagraphic@gmail.com>
2020-03-16 20:31:55 -0400

    🚧 IN PROGRESS: setup with mango-ticker, but runs items one at a time...

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,38 @@
-const news = async () => {
-  let response = await fetch('http://localhost:3000/api/fakenews');
-  let data = await response.json();
-  return data;
-}
+"use strict";
 
 
-
-news()
-  .then(data => {
-    renderNewsPage(data.news);
-  })
-  .catch(error => {
-    new Error(error);
-  });
+startTicker('news-ticker', {speed:1, delay:0});
 
 
-function renderNewsPage(news) {
-
-  for (const prop in news) {
-    if (prop !== 'ALL') {
-      console.log(news[prop]);
-    }
-  }
-
-}
-
-
-function renderNewsTicker() {
-
-}
+//
+// const news = async () => {
+//   let response = await fetch('http://localhost:3000/api/fakenews');
+//   let data = await response.json();
+//   return data;
+// }
+//
+//
+//
+// news()
+//   .then(data => {
+//     renderNewsPage(data.news);
+//   })
+//   .catch(error => {
+//     new Error(error);
+//   });
+//
+//
+// function renderNewsPage(news) {
+//
+//   for (const prop in news) {
+//     if (prop !== 'ALL') {
+//       console.log(news[prop]);
+//     }
+//   }
+//
+// }
+//
+//
+// function renderNewsTicker() {
+//
+// }
