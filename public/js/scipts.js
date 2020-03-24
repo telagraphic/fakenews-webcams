0852ec8167bdd9792f519378135b17fbc37ae361
telagraphic <telagraphic@gmail.com>
2020-03-24 12:01:50 -0400

    🚧 IN PROGRESS: troubleshooting marquee start on fetch request

--- public/js/scipts.js
+++ public/js/scipts.js
@@ -15,14 +15,14 @@ news()
   });
 
 
-// function renderNewsTicker(news) {
-//   const newsTickerStories = '';
-//   const newsTickerElement = document.querySelector('.ticker__feed');
-//
-//   let headlines = news.map(story => story.headline).join("\xa0\xa0\xa0\xa0\xa0");
-//   newsTickerElement.innerText = headlines;
-// }
-//
+function renderNewsTicker(news) {
+  const newsTickerStories = '';
+  const newsTickerElement = document.querySelector('.ticker__feed');
+
+  let headlines = news.map(story => story.headline).join("\xa0\xa0\xa0\xa0\xa0");
+  newsTickerElement.innerText = headlines;
+}
+
 if (window.innerWidth > 800) {
   nodeMarquee({
       selector: '.ticker__feed',
