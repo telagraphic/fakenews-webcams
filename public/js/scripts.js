b696ee712e198cf5897df6ac22b2c009e61851a1
telagraphic <telagraphic@gmail.com>
2020-03-24 11:41:15 -0400

    🚧 IN PROGRESS: troubleshooting marquee start on fetch request

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,20 +1,7 @@
 import nodeMarquee from './node-marquee.js';
 
-if (window.innerWidth > 800) {
-  nodeMarquee({
-      selector: '.ticker__feed',
-      speed: 3
-  });
-} else {
-  nodeMarquee({
-      selector: '.ticker__feed',
-      speed: 1.5
-  });
-}
-
 const news = async () => {
   let response = await fetch('http://localhost:3000/api/fakenews');
-  // let response = await fetch('http://fakenewswebcams.com/api/fakenews');
   let data = await response.json();
   return data;
 }
@@ -22,7 +9,7 @@ const news = async () => {
 news()
   .then(data => {
     renderNewsPage(data.news);
-    renderNewsTicker(data.news.ALL);
+    renderNewsTicker(data.news.ZEROHEDGE);
   })
   .catch(error => {
     new Error(error);
@@ -44,12 +31,18 @@ function renderNewsTicker(news) {
 
   let headlines = news.map(story => story.headline).join("\xa0\xa0\xa0\xa0\xa0");
   newsTickerElement.innerText = headlines;
-
-  startNewsTicker();
 }
 
-function startNewsTicker() {
 
+
+if (window.innerWidth > 800) {
+  nodeMarquee({
+      selector: '.ticker__feed',
+      speed: 3
+  });
+} else {
+  nodeMarquee({
+      selector: '.ticker__feed',
+      speed: 1.5
+  });
 }
-
-startNewsTicker();
