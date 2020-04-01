a324f7b118ed712af0ae56553df0bbbc1b3f54f8
telagraphic <telagraphic@gmail.com>
2020-03-31 20:49:01 -0400

    🖥 FRONT-END GENERAL: added access-control-allow-origin headers

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ const corsOptions = {
   optionsSuccessStatus: 200
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://fakenewswebcams.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
